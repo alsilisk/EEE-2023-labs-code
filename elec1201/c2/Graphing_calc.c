b1b62fc760d110a1b6540dc891d41f41a2cd7a93
John Doe <alsilisk@icloud.com>
2023-10-11 22:33:43 +0100

    added graphing calculator for lab c2

---
+++ elec1201/c2/Graphing_calc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+#include <windows.h>
+#include "graphing_calc.h"
+
+void plotval(float y, unsigned long x, int tick_mark, char* plot_line, int previous_char, int column){
+    char* tickmark[] = {"---","   "};
+    plot_line[previous_char] = ' ';
+    plot_line[column] = '*';
+    printf(CYN "x = %010d" RED ", " MAG "y = %03f" RED "|%s" YEL "%s\n" RESET, x, y, tickmark[tick_mark], plot_line);
+}
+
+
+int main() {
+    unsigned long x = 0ul;
+    float y = 0;
+    int rationalise = 0;
+    int previous_char; 
+    int term_width = console_size();
+    int tick_mark;
+
+    char *plot_line = (char*)malloc(term_width-32);
+    memset(plot_line,' ',term_width - 33);
+    plot_line[term_width - 33] = '\0';
+    for(x = 0; x < 100; x++) {
+        y = (sin(((x*7) * 3.14159)/180)/2 + 0.5);
+        previous_char = rationalise;
+        rationalise = (term_width - 34) * y;
+        if( x % 5 == 0){
+            tick_mark = 0;
+        }
+        plotval(y, x, tick_mark, plot_line, previous_char, rationalise);
+        tick_mark = 1;
+    }
+}
---
+++ elec1201/c2/Graphing_calc.h
@@ -0,0 +1,25 @@
+#include <windows.h>
+
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+#ifdef __linux__
+    static inline int console_size() {
+        struct winsize w;
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+        return w.ws_row;
+    }
+#elif defined(_WIN32)
+    static inline int console_size() {
+        int ret;
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        ret = GetConsoleScreenBufferInfo(GetStdHandle( STD_OUTPUT_HANDLE ),&csbi);
+        return csbi.dwSize.X;
+    }
+#endif
